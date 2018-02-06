37706c77b91bb21db3d0108ecf21dd0b9c6d258e
Frank Taillandier <frank.taillandier@gmail.com>
2018-02-06 14:43:45 +0100

    Use minima theme

--- generate_site.sh
+++ generate_site.sh
@@ -8,36 +8,122 @@ function die()
 [[ $# -ne 2 ]] && die "Not enough arguments"
 POST_COUNT=$1
 SITE_DIR=$2
-export POST_CONTENT=`cat <<EOF
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem leo, lacinia id consequat sit amet, interdum nec erat. Suspendisse dapibus tristique libero ac tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis est sapien, gravida et lobortis ac, pharetra sit amet diam. Vivamus at sapien a mauris consequat interdum at pharetra sem. Phasellus ultricies, nisi a mattis ornare, odio metus auctor velit, ac aliquam ante ipsum id sem. Nullam sodales tortor commodo purus bibendum pharetra.
 
-Quisque a molestie urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla velit velit, luctus ut nisi a, porta consequat tellus. Fusce finibus massa in arcu rhoncus, vel suscipit nisl cursus. Nunc sodales ligula sed ante dapibus tincidunt. Nunc vestibulum mattis urna, sodales tristique lorem imperdiet vel. Curabitur euismod dignissim aliquet. Fusce non feugiat justo, vel bibendum sem. Curabitur dictum, massa eget cursus mattis, augue libero tincidunt sem, eget laoreet magna enim ut arcu.
+export POST_CONTENT=`cat <<EOF
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum neque
+eget nunc mattis eu sollicitudin enim tincidunt. Vestibulum lacus tortor,
+ultricies id dignissim ac, bibendum in velit.
+
+## Some great heading (h2)
+
+Proin convallis mi ac felis pharetra aliquam. Curabitur dignissim accumsan
+rutrum. In arcu magna, aliquet vel pretium et, molestie et arcu.
+
+Mauris lobortis nulla et felis ullamcorper bibendum. Phasellus et hendrerit
+mauris. Proin eget nibh a massa [vestibulum pretium][ref]. Suspendisse eu nisl a ante
+aliquet bibendum quis a nunc. Praesent varius interdum vehicula. Aenean risus
+libero, placerat at vestibulum eget, ultricies eu enim. Praesent nulla tortor,
+malesuada adipiscing adipiscing sollicitudin, adipiscing eget est.
+
+## Another great heading (h2)
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum neque
+eget nunc mattis eu sollicitudin enim tincidunt. Vestibulum lacus tortor,
+ultricies id dignissim ac, bibendum in velit.
+
+### Some great subheading (h3)
+
+Proin convallis mi ac felis pharetra aliquam. Curabitur dignissim accumsan
+rutrum. In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis
+nulla et felis ullamcorper bibendum.
+
+Phasellus et hendrerit mauris. Proin eget nibh a massa vestibulum pretium.
+Suspendisse eu nisl a ante aliquet bibendum quis a nunc.
+
+### Some great subheading (h3)
+
+Praesent varius interdum vehicula. Aenean risus libero, placerat at vestibulum
+eget, ultricies eu enim. Praesent nulla tortor, malesuada adipiscing adipiscing
+sollicitudin, adipiscing eget est.
+
+> This quote will change your life. It will reveal the secrets of the universe,
+and all the wonders of humanity. Don't misuse it.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum neque
+eget nunc mattis eu sollicitudin enim tincidunt.
+
+### Some great subheading (h3)
+
+Vestibulum lacus tortor, ultricies id dignissim ac, bibendum in velit. Proin
+convallis mi ac felis pharetra aliquam. Curabitur dignissim accumsan rutrum.
+
+{% highlight ruby %}
+def foo
+  puts 'foo'
+end
+{% endhighlight %}
+
+In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis nulla
+et felis ullamcorper bibendum. Phasellus et hendrerit mauris.
+
+#### You might want a sub-subheading (h4)
+
+In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis nulla
+et felis ullamcorper bibendum. Phasellus et hendrerit mauris.
+
+In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis nulla
+et felis ullamcorper bibendum. Phasellus et hendrerit mauris.
+
+#### But it's probably overkill (h4)
+
+In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis nulla
+et felis ullamcorper bibendum. Phasellus et hendrerit mauris.
+
+### Oh hai, an unordered list!!
+
+In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis nulla
+et felis ullamcorper bibendum. Phasellus et hendrerit mauris.
+- First item, yo
+- Second item, dawg
+- Third item, what what?!
+- Fourth item, fo sheezy my neezy
+
+### Oh hai, an ordered list!!
+
+In arcu magna, aliquet vel pretium et, molestie et arcu. Mauris lobortis nulla et felis ullamcorper bibendum. Phasellus et hendrerit mauris.
+
+1. First item, yo
+2. Second item, dawg
+3. Third item, what what?!
+4. Fourth item, fo sheezy my neezy
+
+Quisque a molestie urna. [Vestibulum ante][ref] ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla velit velit, luctus ut nisi a, porta consequat tellus. Fusce finibus massa in arcu rhoncus, vel suscipit nisl cursus. Nunc sodales ligula sed ante dapibus tincidunt. Nunc vestibulum mattis urna, sodales tristique lorem imperdiet vel. Curabitur euismod dignissim aliquet. Fusce non feugiat justo, vel bibendum sem. Curabitur dictum, massa eget cursus mattis, augue libero tincidunt sem, eget laoreet magna enim ut arcu.
+
+[ref]: https://jekyllrb.com
+
 EOF`
 
 POST_DIR=${SITE_DIR}/_posts
-LAYOUT_DIR=${SITE_DIR}/_layouts
 mkdir -p $POST_DIR
-mkdir -p $LAYOUT_DIR
-
-cp ./default_layout.html $LAYOUT_DIR/default.html
-cp ./post_layout.html $LAYOUT_DIR/post.html
 
 cat >${SITE_DIR}/_config.yml << EOF
 url: https://example.com
-baseurl: baseURL
+baseurl: /baseURL
+theme: minima
+EOF
+
+cat >${SITE_DIR}/Gemfile << EOF
+gem "jekyll", git: "https://github.com/jekyll/jekyll"
+gem "minima"
 EOF
 
 cat >${SITE_DIR}/index.html << EOF
 ---
-layout: default
+layout: home
 title: Jekyll Test Site
 ---
-<h1>Blog Posts</h1>
-<ul class="posts">
-    {% for post in site.posts %}
-    <li><span>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ post.url }}">{{ post.title }}</a></li>
-    {% endfor %}
-</ul>
+
 EOF
 
 for post_number in `seq 1 $POST_COUNT`
@@ -47,6 +133,7 @@ do
     post_file="$post_date-test_post_$post_number"
     cat >$POST_DIR/$post_file.md << EOF
 ---
+layout: post
 title: Testing Post $POST_NUMBER
 ---
 
@@ -54,4 +141,3 @@ $POST_CONTENT
 EOF
 
 done
-
